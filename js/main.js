51fb6884376443dd3b6105ab1c787652ed3bc6e8
jake-steinberg <j.steinberg@live.com>
2022-04-04 20:18:12 -0500

    Activity 9

--- js/main.js
+++ js/main.js
@@ -1,148 +1,87 @@
-    //SVG dimension variables
-    var w = 900, h = 500;
+//begin script when window loads
+window.onload = setMap();
 
-//execute script when window is loaded
-window.onload = function(){
+//set up choropleth map
+function setMap(){
+    //map frame dimensions
+    var width = 960,
+        height = 760;
 
-    var container = d3.select("body") //get the <body> element from the DOM
-        .append("svg") //put a new svg in the body
-        .attr("width", w) //assign the width
-        .attr("height", h) //assign the height
-        .attr("class", "container") //always assign a class (as the block name) for styling and future selection
-        .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
+    //create new svg container for the map
+    var map = d3.select("body")
+    .append("svg")
+    .attr("class", "map")
+    .attr("width", width)
+    .attr("height", height);
+
+    //create projection centered on MSP.
+    var projection = d3.geoAlbers()
+    .center([.12, 44.98])
+    .rotate([93.26, 0, 0])
+    .parallels([44.96, 44.96])
+    .scale(48000)
+    .translate([width / 2, height / 2]);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [];
+    promises.push(d3.csv("data/msp_data.csv"));                    
+    promises.push(d3.json("data/msp_geo.json")); 
+    promises.push(d3.json("data/rivers.json"));                   
+    promises.push(d3.json("data/lakes.json")); 
+    promises.push(d3.json("data/interstates.json"));       
+    promises.push(d3.json("data/roads.json"));                                                                   
+    Promise.all(promises).then(callback);
+
+    function callback(data) {
+        var mspData = data[0],
+            mspGeo = data[1];
+            rivers = data[2]
+            lakes = data[3]
+            interstates = data[4]
+            roads = data[5]
+
+        //translate TopoJSON
+        var mspGeo = topojson.feature(mspGeo, mspGeo.objects.msp_geo).features,
+            rivers = topojson.feature(rivers, rivers.objects.rivers);
+            lakes = topojson.feature(lakes, lakes.objects.lakes);
+            interstates = topojson.feature(interstates, interstates.objects.interstates);
+            roads = topojson.feature(roads, roads.objects.roads);
+
+        //add rivers to map
+        var rivers = map.append("path")
+        .datum(rivers)
+        .attr("class", "rivers")
+        .attr("d", path);
+
+        //add lakes to map
+        var lakes = map.append("path")
+        .datum(lakes)
+        .attr("class", "lakes")
+        .attr("d", path);
         
-    //innerRect block
-    var innerRect = container.append("rect") //put a new rect in the svg
-        .datum(400) //a single value is a datum
-        .attr("width", function(d){ //rectangle width
-            return d * 2; //400 * 2 = 800
-        }) 
-        .attr("height", function(d){ //rectangle height
-            return d; //400
-        })
-        .attr("class", "innerRect") //class name
-        .attr("x", 50) //position from left on the x (horizontal) axis
-        .attr("y", 50) //position from top on the y (vertical) axis
-        .style("fill", "#FFFFFF"); //fill color
+        //add interstates to map
+        var interstates = map.append("path")
+        .datum(interstates)
+        .attr("class", "interstates")
+        .attr("d", path);
 
-    var cityPop = [
-        { 
-            city: 'Madison',
-            population: 233209
-        },
-        {
-            city: 'Milwaukee',
-            population: 594833
-        },
-        {
-            city: 'Green Bay',
-            population: 104057
-        },
-        {
-            city: 'Superior',
-            population: 27244
-        }
-    ];
+        //add roads to map
+        var roads = map.append("path")
+        .datum(roads)
+        .attr("class", "roads")
+        .attr("d", path);
 
-    var x = d3.scaleLinear() //create the scale
-        .range([90, 810]) //output min and max
-        .domain([0, 3.5]); //input min and max
-
-    //find the minimum value of the array
-    var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
-
-    //find the maximum value of the array
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
-
-    //scale for circles center y coordinate
-    var y = d3.scaleLinear()
-        .range([450, 50]) //was 440, 95
-        .domain([0, 700000]); //was minPop, maxPop
-        ;
-
-    //color scale generator 
-    var color = d3.scaleLinear()
-        .range([
-            "#FDBE85",
-            "#D94701"
-        ])
-        .domain([
-            minPop, 
-            maxPop
-        ]);
-    
-    var circles = container.selectAll(".circles") //but wait--there are no circles yet!
-        .data(cityPop) //here we feed in an array
-        .enter() //one of the great mysteries of the universe
-        .append("circle") //add a circle for each datum
-        .attr("class", "circles") //apply a class name to all circles
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d, i){ //circle radius
-            //calculate the radius based on population value as circle area
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d, i){
-            //use the scale generator with the index to place each circle horizontally
-            return x(i);
-        })
-        .attr("cy", function(d){
-            return y(d.population);
-        })
-        .style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-        })
-        .style("stroke", "#000"); //black circle stroke
-
-        var yAxis = d3.axisLeft(y);
-
-        //create axis g element and add axis
-        var axis = container.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(50, 0)")
-            .call(yAxis);
-        
-        yAxis(axis);
-
-        var labels = container.selectAll(".labels")
-        .data(cityPop)
-        .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "left")
-        .attr("y", function(d){
-            //vertical position centered on each circle
-            return y(d.population) + -3;
-        });
-
-    //first line of label
-    var nameLine = labels.append("tspan")
-        .attr("class", "nameLine")
-        .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .text(function(d){
-            return d.city;
-        });
-
-    //create format generator
-    var format = d3.format(",");
-
-    //Example 3.16 line 1...second line of label
-    var popLine = labels.append("tspan")
-        .attr("class", "popLine")
-        .attr("x", function(d,i){
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "15") //vertical offset
-        .text(function(d){
-            return "Pop. " + format(d.population); //use format generator to format numbers
-        });
-    };
+        //add municipalities to map
+        var municipalities = map.selectAll(".municipalities")
+            .data(mspGeo)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "municipalities " + d.properties.AFFGEOID;
+            })
+            .attr("d", path);
+   }
+};
